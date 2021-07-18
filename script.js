8eda345923e976d0221db78f771b278e0aedcb07
alfadlims <alfadli20001@mail.unpad.ac.id>
2021-07-18 23:38:22 +0700

    konten faq dan js modal

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ function navbarcollapse() {
     nav.classList.toggle("collapse-bg");
   });
 }
+
+function modal() {
+  var container = document.querySelector(".grid-history");
+  var modalImage = document.querySelector(".tmst");
+
+  container.addEventListener("click", function (e) {
+    console.log(e.target.className);
+    if (e.target.classList.contains("logo2016")) {
+      modalImage.src = "img/tmst2016.jpg";
+    } else if (e.target.classList.contains("logo2018")) {
+      modalImage.src = "img/tmst2018.jpeg";
+    } else if (e.target.classList.contains("logo2019")) {
+      modalImage.src = "img/tmst2019.jpg";
+    } else if (e.target.classList.contains("logo2020")) {
+      modalImage.src = "img/tmst2020.jpg";
+    }
+  });
+}
