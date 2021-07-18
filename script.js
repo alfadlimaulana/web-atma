85c6f0d6f80991cc90b5bf4acc1f7b88de8288db
alfadlims <alfadli20001@mail.unpad.ac.id>
2021-07-18 15:42:39 +0700

    rubah video source

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
 function navbarbg() {
   var nav = document.querySelector("nav");
 
-  window.addEventListener("scroll", function () {
+  document.addEventListener("scroll", function () {
     if (this.window.pageYOffset > 10) {
       nav.classList.add("navbar-bg", "shadow");
     } else {
       nav.classList.remove("navbar-bg", "shadow");
     }
   });
-}
-
-function navbarcollapse() {
-  var button = document.querySelector(".navbar-toggler");
-
-  if (this.window.pageYOffset <= 100) {
-    button.addEventListener("click", function () {
-      if (nav.classList.contains("navbar-bg")) {
-        nav.classList.remove("navbar-bg", "shadow");
-      } else {
-        nav.classList.add("navbar-bg", "shadow");
-      }
-    });
-  }
 }
