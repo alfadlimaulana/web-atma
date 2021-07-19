66244ea234f9665fc0bca45e1e4d411a03f39631
Wafi Fahruzzaman <wafi20002@mail.unpad.ac.id>
2021-07-19 18:37:48 +0700

    progres

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function navbarbg() {
   var nav = document.querySelector("nav");
 
   document.addEventListener("scroll", function () {
-    if (this.window.pageYOffset > 10) {
+    if (window.pageYOffset > 10) {
       nav.classList.add("navbar-bg", "shadow");
     } else {
       nav.classList.remove("navbar-bg", "shadow");
