f0f91eea8cdd49016940207db59dd642ea85b166
alfadlims <alfadli20001@mail.unpad.ac.id>
2021-07-21 18:46:09 +0700

    modal script (belum artist)

--- script.js
+++ script.js
@@ -21,18 +21,33 @@ function navbarcollapse() {
 
 function modal() {
   var container = document.querySelector(".grid-history");
-  var modalImage = document.querySelector(".tmst");
+  var modalImage = document.querySelector(".border-grad img");
+  var date = document.querySelector(".list-unstyled li:nth-child(1)");
+  var venue = document.querySelector(".list-unstyled li:nth-child(2)");
+  var theme = document.querySelector(".list-unstyled li:nth-child(3)");
 
   container.addEventListener("click", function (e) {
     console.log(e.target.className);
     if (e.target.classList.contains("logo2016")) {
       modalImage.src = "img/tmst2016.jpg";
+      date.innerHTML = "<strong>Date :</strong> January 9, 2016";
+      venue.innerHTML = "<strong>Venue :</strong> Sasana Budaya Ganesha, Bandung";
+      theme.innerHTML = "<strong>Theme :</strong> Bandung";
     } else if (e.target.classList.contains("logo2018")) {
       modalImage.src = "img/tmst2018.jpeg";
+      date.innerHTML = "<strong>Date :</strong> September 29, 2018";
+      venue.innerHTML = "<strong>Venue :</strong> Lapangan Pussenif, Bandung";
+      theme.innerHTML = "<strong>Theme :</strong> HIFESTUS";
     } else if (e.target.classList.contains("logo2019")) {
       modalImage.src = "img/tmst2019.jpg";
+      date.innerHTML = "<strong>Date :</strong> October 12, 2019";
+      venue.innerHTML = "<strong>Venue :</strong> Lapangan Pussenif, Bandung";
+      theme.innerHTML = "<strong>Theme :</strong> RATETRA";
     } else if (e.target.classList.contains("logo2020")) {
       modalImage.src = "img/tmst2020.jpg";
+      date.innerHTML = "<strong>Date :</strong> November 20, 2020";
+      venue.innerHTML = "<strong>Venue :</strong> Virtual Concert";
+      theme.innerHTML = "<strong>Theme :</strong> RHEMURA";
     }
   });
 }
